bb4817768bd1fa16492255b07af1e1842061e43c
Shubham Mahant <shubhammahant369@gmail.com>
2026-08-14 16:52:54 +0530

    fix: next.config.js with ignoreBuildErrors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,1 +1,10 @@
-module.exports = { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+module.exports = nextConfig
